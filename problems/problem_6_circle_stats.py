db8f11b43fa418bdbe967ed92f071c411c8cd2f1
Ana Herrera Flores <ana.herreraflores@g.austincc.edu>
2026-01-02 19:42:14 +0000

    problem 6 done!

--- problems/problem_6_circle_stats.py
+++ problems/problem_6_circle_stats.py
@@ -3,25 +3,28 @@ Given a radius:
   - print the circumference and area of a circle
   - print the surface area and volume of a sphere
 """
+from math import pi
+
+
 def circumference(r):
-    pass
+    return 2 * pi * r
 
 
 def area(r):
-    pass
+    return pi * r ** 2
 
 
 def surface_area(r):
-    pass
+    return 4 * pi * r ** 2
 
 
 def volume(r):
-    pass
+    return (4/3) * pi * r ** 3
 
 
 radius = float(input("Radius: "))
 
-print('Circle circumference: {}')
-print('Circle area: {}')
-print('Sphere surface area: {}')
-print('Sphere volume: {}')
+print(f'Circle circumference: {circumference(radius)}')
+print(f'Circle area: {area(radius)}')
+print(f'Sphere surface area: {surface_area(radius)}')
+print(f'Sphere volume: {volume(radius)}')
