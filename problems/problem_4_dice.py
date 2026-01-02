cc940ce576a332b08111105320de30f0c8960417
Ana Herrera Flores <ana.herreraflores@g.austincc.edu>
2026-01-02 19:01:27 +0000

    problem 4 done!

--- problems/problem_4_dice.py
+++ problems/problem_4_dice.py
@@ -3,5 +3,9 @@ Simulate rolling 2 dice
 """
 
 # Todo: Get two random integers between 1 and 6 (inclusive)
+import random
 
-print("You rolled a {} and {} (total: {})")
+value_1 = random.randint(1, 6)
+value_2 = random.randint(1, 6)
+
+print(f"You rolled a {value_1} and {value_2} (total: {value_1 + value_2})")
