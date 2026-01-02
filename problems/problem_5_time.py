3bf2c19c401e3fdfda509e95d0ea79b50d493a1c
Ana Herrera Flores <ana.herreraflores@g.austincc.edu>
2026-01-02 19:30:26 +0000

    problem 5 done!

--- problems/problem_5_time.py
+++ problems/problem_5_time.py
@@ -1,7 +1,14 @@
 """
 Print the elapsed time
 """
+
+import time
+
+start = time.time()
+print(start)
 input("Press enter to START the timer: ")
 input("Press enter to STOP the timer: ")
+end = time.time()
+elapsed = end - start
 
-print("{}s elapsed")
+print(f"Elapsed time: {elapsed} seconds")
